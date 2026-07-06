36e0b248939b160c3cfed63a3120133af25f93f2
alanbong <Denis Bochkarev>
2026-07-06 19:50:08 +0300

    add pacman and brew packages

--- link_linux.sh
+++ link_linux.sh
@@ -12,3 +12,5 @@ source "$script_dir/link_core.sh"
 
 # Link universal dotfiles
 source "$script_dir/link_universal.sh"
+
+sudo pacman -S --needed --noconfirm base-devel nodejs npm xclip wl-clipboard
--- link_mac.sh
+++ link_mac.sh
@@ -12,3 +12,18 @@ source "$script_dir/link_core.sh"
 
 # Link universal dotfiles
 source "$script_dir/link_universal.sh"
+
+if ! command -v brew &>/dev/null; then
+    echo "Homebrew not found. Installing:"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    
+    if [[ "$(uname -m)" == "arm64" ]]; then
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    fi
+else
+    echo "Homebrew installed."
+fi
+
+if ! brew list node &>/dev/null; then
+    brew install node
+fi
